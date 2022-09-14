68bbc803c203ba4ec130c0b048c911bb54239b3f
adiletakmatov <adiletakmatovofficial@gmail.com>
2022-09-15 02:41:40 +0600

    fix: edit module.js fix bag

--- server/models/models.js
+++ server/models/models.js
@@ -21,7 +21,7 @@ const Device = sequelize.define('Device',{
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement:true},
     name:{type: DataTypes.STRING, unique:true, allowNull: false},
     price: {type: DataTypes.INTEGER, allowNull: false},
-    rating: {type: DataTypes.INTEGER, defaultValue: 0}
+    rating: {type: DataTypes.INTEGER, defaultValue: 0},
     img: {type: DataTypes.STRING, allowNull: false},
 })
 
